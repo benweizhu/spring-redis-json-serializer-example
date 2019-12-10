e5508fd4b2ac871886d74f1e69b4b73948a53046
benweizhu <benwei.zhu@exxonmobil.com>
2019-12-10 13:26:31 +0800

    init

---
+++ src/main/java/me/zeph/spring/redis/json/springredisjson/SpringRedisJsonApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.spring.redis.json.springredisjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisJsonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRedisJsonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/zeph/spring/redis/json/springredisjson/SpringRedisJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeph.spring.redis.json.springredisjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisJsonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
